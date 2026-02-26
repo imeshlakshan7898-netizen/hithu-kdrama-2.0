e5938d085c585b121d949a39d711df15db7ff26f
Imesh Lakshan <imeshlakshan7889@gmail.com>
2026-02-26 16:22:31 +0530

    update sitemap to .com.lk domain

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://hithu-kdrama-2-0.vercel.app',
+      url: 'https://hithukdrama.com.lk',
       lastModified: new Date(),
       changeFrequency: 'daily',
       priority: 1,
